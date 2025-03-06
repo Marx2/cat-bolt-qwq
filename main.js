6e803e29f9a10b52626c4f57c13113da5eb35629
Marx <mnazarko@gmail.com>
2025-03-06 22:28:06 +0100

    changes

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
+// Updated button placement and text
 document.querySelector('#app').innerHTML = `
-      <div>
-        <h1>Random HTTP Cat Viewer</h1>
-        <button id="randomButton" type="button">Generate Random Status Code</button>
-        <img id="catImage" src="" alt="HTTP Cat Image">
-      </div>
-    `;
-    
-    const randomButton = document.getElementById('randomButton');
-    const catImage = document.getElementById('catImage');
+  <div>
+    <button id="randomButton" type="button">Random Error Cat</button>
+    <h1>HTTP Status Code Viewer</h1>
+    <img id="catImage" src="" alt="HTTP Cat Image">
+  </div>
+`;
 
-    randomButton.addEventListener('click', () => {
-      const statusCodes = [200, 403, 404, 500];
-      const code = statusCodes[Math.floor(Math.random() * statusCodes.length)];
-      catImage.src = `https://http.cat/${code}.jpg`;
-    });
+const randomButton = document.getElementById('randomButton');
+const catImage = document.getElementById('catImage');
+
+randomButton.addEventListener('click', () => {
+  const statusCodes = [200, 403, 404, 500];
+  const code = statusCodes[Math.floor(Math.random() * statusCodes.length)];
+  catImage.src = `https://http.cat/${code}.jpg`;
+});
